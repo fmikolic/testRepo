a625ab10a1365f2e180c54a31e80c93aafcc3b9c
fmikolic <mikolicfran@gmail.com>
2022-01-27 10:08:08 +0100

    hashed password done

--- www/data/config.php
+++ www/data/config.php
@@ -1,4 +1,4 @@
 <?php
 const USERNAME='demo';
-const PASSWORD='demo';
+const PASSWORD='$2y$10$aRVaJT9R2qaRj6d7ZJZ.YupAlGofIkQzsCfP18qnOdLZFQ.T7eQbu';
 
--- www/login.php
+++ www/login.php
@@ -5,11 +5,10 @@ require './data/config.php';
 
 $username1= $_POST['username'];
 $password1=$_POST['password'];
-if(!($username1===USERNAME && $password1===PASSWORD)){
+if(!($username1===USERNAME) && password_hash($password1, PASSWORD_BCRYPT) === PASSWORD){
     echo ('Login failed!');
 }
 else{
-    echo ('proslo');
     $_SESSION['username']= $username1;
     $_SESSION['password']=$password1;
     header("Location: ./secret.php");
