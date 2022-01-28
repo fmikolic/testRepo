2179435cbbcb517721c2d7f1e0665cf3996a96c3
fmikolic <mikolicfran@gmail.com>
2022-01-28 10:59:08 +0100

    display all users for admin

--- www/admin/users.php
+++ www/admin/users.php
@@ -7,49 +7,50 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Secret tab</title>
+    <title>ADMIN tab</title>
 </head>
 <body class="text-center">
 <header>
     <?php
-    include ('header.php');
+    include ('../header.php');
     ?>
 </header>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<h2 class="h2 mb-3 font-weight-normal">SECRET UNUTRA</h2>
+<h2 class="h2 mb-3 font-weight-normal">ADMIN UNUTRA</h2>
 <div class="mx-auto" style="width: 25%" >
     <?php
-    require './data/helper.php';
-    require './data/config.php';
+    require '../data/helper.php';
+    require '../data/config.php';
     $connect=openConnection(DBCONNECTIONDATA);
-    $username1=$_SESSION['username'];
-
-    $query="select secret_data.data from secret_data left join users on secret_data.user_id=users.id_user where users.username='".$username1."'";
+    $query="select * from users";
     $result=mysqli_query($connect,$query);
     echo "<table class='table table-dark table-hover'>";
-    echo "<thead><tr><th scope='col'>Data for user</th></tr></thead>";
-        foreach($result as $row){
-            echo "<tr><td>" . $row['data'] . "</td></tr><br>";
-        }
+    echo "<thead><tr><th scope='col'>ID</th>
+<th scope='col'>Username</th>
+<th scope='col'>Password</th></tr></thead>";
+    foreach($result as $row){
+        echo "<tr></tr><td>" . $row['id_user'] . "</td><br>";
+        echo "<td>" . $row['username'] . "</td><br>";
+        echo "<td>" . $row['password'] . "</td></tr><br>";
+    }
     echo "</table>";
 
     closeConnection($connect);
     ?>
 </div>
 
-<button class="btn" onclick="window.location.href='admin/users.php'" type="button">Admin page</button>
-
 <footer>
     <?php
-    include ('footer.php');
+    include ('../footer.php');
 
     ?>
 </footer>
 
 </body>
 </html>
+
 
 
 
