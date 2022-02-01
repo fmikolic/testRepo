ac9e1f97d93483234725192497faee011f6f1aa1
fmikolic <mikolicfran@gmail.com>
2022-02-01 15:23:58 +0100

    added back button

--- www/footer.php
+++ www/footer.php
@@ -1,5 +1,6 @@
 <?php
 ?>
+
 <div class="mt-auto fixed-bottom">
     <div>
         <img src="imgs/packaging.png" alt width="144" height="144">
@@ -7,6 +8,10 @@
     <div>
         <p class="mt-3 mb-3 text-muted">© 2022 Vježba</p>
     </div>
-</>
+    <div style="display: flex; justify-content: left">
+        <a type="button" href="">troll</a>
+    </div>
+</div>
+
 
 
--- www/header.php
+++ www/header.php
@@ -2,8 +2,11 @@
 
 ?>
 <body style="background-image: url('imgs/pexels-isaac-garcia-9567687.jpg');background-size: cover; background-repeat: no-repeat; background-attachment: fixed; background-position: center top">
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light " style="opacity: 0.67">
     <div>
+        <div style="display:flex;align-content: flex-start">
+            <input class="btn btn-info " type="button" value="Back" onclick="history.go(-1)">
+        </div>
         <h2 class="display-4 font-weight-normal"> Inspirativni tekst </h2>
         <p class="lead font-weight-normal text-break">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus accumsan iaculis. Nam placerat ultricies sem ut interdum. Quisque nec nisi a velit convallis varius. Aliquam erat volutpat. Praesent vulputate, nisl sit amet imperdiet sodales, sapien nisi luctus ante, ut malesuada felis sapien id nisi. Nulla facilisi. Aenean id felis varius velit cursus ullamcorper vel feugiat urna. </p>
     </div>
